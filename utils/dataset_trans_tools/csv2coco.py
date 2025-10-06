15263fc7a75a9ec2894325533ef15b861d5d39d6
Huye-Zhong <108203370+wshr123@users.noreply.github.com>
2025-10-06 11:27:59 +0800

    Update csv2coco.py

--- utils/dataset_trans_tools/csv2coco.py
+++ utils/dataset_trans_tools/csv2coco.py
@@ -7,227 +7,11 @@ from tqdm import tqdm
 import itertools
 import argparse
 
-# def csv2COCOJson_dynamic(csv_path, movie_list, img_root, json_path, min_json_path):
-#
-#     # 读取 CSV
-#     ann_df = pd.read_csv(csv_path, header=None)
-#     """
-#     按照 AVA 的 CSV，假设其列顺序为：
-#     0: movie_name
-#     1: timestamp
-#     2: x1
-#     3: y1
-#     4: x2
-#     5: y2
-#     6: action_id
-#     7: person_id
-#     如果列顺序不同，可自行修改。
-#     """
-#
-#     # 构建 movie_ids，用于后续为每个 movie 分配唯一的起始编号
-#     movie_ids = {}
-#     with open(movie_list, 'r') as f:
-#         for idx, line in enumerate(f):
-#             # 假设文件名格式为: something.mp4
-#             # 去掉后缀，只保留名字
-#             name = line.strip()
-#             dot_pos = name.rfind('.')
-#             if dot_pos > 0:
-#                 name = name[:dot_pos]
-#             movie_ids[name] = idx
-#
-#     # 存储所有视频的图像及注释信息： { movie_name: { 'img_infos': { timestamp: {...} }, 'size': (w, h) } }
-#     movie_infos = {}
-#
-#     # 用于对 annotation 分配唯一 ID
-#     global_ann_id = 1
-#
-#     # 遍历 CSV，每行都做一次处理
-#     for row in tqdm(ann_df.itertuples(), total=len(ann_df), desc="Processing CSV"):
-#         # 按自己的列索引取值
-#         _, movie_name, timestamp, x1, y1, x2, y2, action_id, person_id = row
-#         movie_name = str(movie_name)
-#         timestamp = timestamp * 30
-#         # 如果第一次遇到该 movie_name，先创建其信息
-#         if movie_name not in movie_infos:
-#             movie_infos[movie_name] = {
-#                 'img_infos': {},
-#                 'size': None  # 后面第一次读到对应帧图像时再确定
-#             }
-#
-#         # 如果第一次遇到该 timestamp，先创建图像信息
-#         if timestamp not in movie_infos[movie_name]['img_infos']:
-#             # 构造图像路径 (按你真实的目录结构调整)
-#             img_fname = f"img_{timestamp:05d}.jpg"
-#             img_full_path = os.path.join(img_root, movie_name, img_fname)
-#
-#             # 如果还未获取到该 movie 的图像尺寸，则读取一下
-#             if movie_infos[movie_name]['size'] is None:
-#                 # 读一张帧，获取宽高 (如有必要，可以 try/except 跳过无法读取的帧)
-#                 with Image.open(img_full_path) as img:
-#                     width, height = img.size
-#                 movie_infos[movie_name]['size'] = (width, height)
-#             else:
-#                 # 如果已经有 size，就直接用
-#                 width, height = movie_infos[movie_name]['size']
-#
-#             # 构造一个稳定的 image_id，防止不同 movie 的 timestamp 冲突
-#             # 这里 movie_ids[movie_name] * 1000000 是为了给该 movie 预留足够大的区间
-#             image_id = movie_ids[movie_name] * 1000000 + timestamp
-#
-#             movie_infos[movie_name]['img_infos'][timestamp] = {
-#                 'id': image_id,
-#                 'file_name': os.path.join(movie_name, img_fname),
-#                 'height': height,
-#                 'width': width,
-#                 'movie': movie_name,
-#                 'timestamp': timestamp,
-#                 'annotations': []  # 用列表存储 annotation，避免被 action_id 覆盖
-#             }
-#
-#         # 从字典中取出当前图像的信息
-#         img_info = movie_infos[movie_name]['img_infos'][timestamp]
-#         width = img_info['width']
-#         height = img_info['height']
-#
-#         # 计算 bounding box
-#         box_w = (x2 - x1) * width
-#         box_h = (y2 - y1) * height
-#         real_x1 = x1 * width
-#         real_y1 = y1 * height
-#         area = box_w * box_h
-#
-#         # 构造一个全局唯一的 annotation_id
-#         ann_id = global_ann_id
-#         global_ann_id += 1
-#
-#         # 新增一个 annotation
-#         img_info['annotations'].append({
-#             'id': ann_id,
-#             'image_id': img_info['id'],
-#             'img_path': os.path.join(movie_name, img_fname),
-#             'category_id': 1,     # 假设只有一个 “person”
-#             'action_ids': [action_id],
-#             'person_id': person_id,
-#             'bbox': [round(real_x1, 2), round(real_y1, 2),
-#                      round(box_w, 2),   round(box_h, 2)],
-#             'area': round(area, 2),
-#             'iscrowd': 0
-#         })
-#
-#     # 构造 COCO 风格字典
-#     tic = time.time()
-#     print("Writing into json file...")
-#
-#     jsondata = {}
-#     # 只有一个类别 person，也可以自行扩展
-#     jsondata['categories'] = [
-#         {
-#             'supercategory': 'person',
-#             'id': 1,
-#             'name': 'person'
-#         }
-#     ]
-#
-#     # 收集所有图像和注释
-#     all_images = []
-#     all_annotations = []
-#     for movie_name, minfo in movie_infos.items():
-#         for timestamp, info in minfo['img_infos'].items():
-#             all_images.append({
-#                 'id': info['id'],
-#                 'file_name': info['file_name'],
-#                 'height': info['height'],
-#                 'width': info['width'],
-#                 'movie': movie_name,
-#                 'timestamp': timestamp
-#             })
-#             all_annotations.extend(info['annotations'])
-#
-#     jsondata['images'] = all_images
-#     jsondata['annotations'] = all_annotations
-#
-#     # 写入 JSON 带缩进
-#     with open(json_path, 'w') as f:
-#         json.dump(jsondata, f, indent=4)
-#     print(f"Write json dataset into json file {json_path} successfully.")
-#
-#     # 写入无缩进版本
-#     with open(min_json_path, 'w') as fmin:
-#         json.dump(jsondata, fmin)
-#     print(f"Write json dataset with no indent into json file {min_json_path} successfully.")
-#     print('Done (t={:0.2f}s)'.format(time.time() - tic))
-#
-# def main():
-#     parser = argparse.ArgumentParser(description="Generate coco format json for AVA.")
-#     parser.add_argument(
-#         "--csv_path",
-#         default="",
-#         help="path to csv file",
-#         type=str,
-#     )
-#     parser.add_argument(
-#         "--movie_list",
-#         required=True,
-#         help="path to movie list",
-#         type=str,
-#     )
-#     parser.add_argument(
-#         "--img_root",
-#         required=True,
-#         help="root directory of extracted key frames",
-#         type=str,
-#     )
-#     parser.add_argument(
-#         "--json_path",
-#         default="",
-#         help="path of output json",
-#         type=str,
-#     )
-#     parser.add_argument(
-#         "--min_json_path",
-#         default="",
-#         help="path of output minimized json",
-#         type=str,
-#     )
-#     args = parser.parse_args()
-#
-#     if args.json_path=="":
-#         if args.csv_path == "":
-#             json_path = "test.json"
-#         else:
-#             dotpos = args.csv_path.rfind('.')
-#             if dotpos < 0:
-#                 csv_name = args.csv_path
-#             else:
-#                 csv_name = args.csv_path[:dotpos]
-#             json_path = csv_name + '.json'
-#     else:
-#         json_path = args.json_path
-#
-#     if args.min_json_path=="":
-#         dotpos = json_path.rfind('.')
-#         if dotpos < 0:
-#             json_name = json_path
-#         else:
-#             json_name = json_path[:dotpos]
-#         min_json_path = json_name + '_min.json'
-#     else:
-#         min_json_path = args.min_json_path
-#
-#     if args.csv_path == "":
-#         genCOCOJson(args.movie_list, args.img_root, json_path, min_json_path)
-#     else:
-#         csv2COCOJson_dynamic(args.csv_path, args.movie_list, args.img_root, json_path, min_json_path)
-#
-# if __name__ == '__main__':
-#     main()
 def csv2COCOJson_dynamic(csv_path, movie_list, img_root, json_path, min_json_path):
 
     # 读取 CSV
     ann_df = pd.read_csv(csv_path, header=None)
     """
-    按照 AVA 的 CSV，假设其列顺序为：
     0: movie_name
     1: timestamp
     2: x1
@@ -236,10 +20,8 @@ def csv2COCOJson_dynamic(csv_path, movie_list, img_root, json_path, min_json_pat
     5: y2
     6: action_id
     7: person_id
-    如果列顺序不同，可自行修改。
     """
 
-    # 构建 movie_ids，用于后续为每个 movie 分配唯一的起始编号
     movie_ids = {}
     with open(movie_list, 'r') as f:
         for idx, line in enumerate(f):
@@ -250,44 +32,32 @@ def csv2COCOJson_dynamic(csv_path, movie_list, img_root, json_path, min_json_pat
                 name = name[:dot_pos]
             movie_ids[name] = idx
 
-    # 存储所有视频的图像及注释信息： { movie_name: { 'img_infos': { timestamp: {...} }, 'size': (w, h) } }
     movie_infos = {}
 
-    # 用于对 annotation 分配唯一 ID
     global_ann_id = 1
 
-    # 遍历 CSV，每行都做一次处理
     for row in tqdm(ann_df.itertuples(), total=len(ann_df), desc="Processing CSV"):
-        # 按自己的列索引取值
         _, movie_name, timestamp, x1, y1, x2, y2, action_id, person_id = row
         movie_name = str(movie_name)
         timestamp = timestamp * 30
 
-        # 如果第一次遇到该 movie_name，先创建其信息
         if movie_name not in movie_infos:
             movie_infos[movie_name] = {
                 'img_infos': {},
                 'size': None  # 后面第一次读到对应帧图像时再确定
             }
 
-        # 如果第一次遇到该 timestamp，先创建图像信息
         if timestamp not in movie_infos[movie_name]['img_infos']:
-            # 构造图像路径 (按你真实的目录结构调整)
             img_fname = f"img_{timestamp:05d}.jpg"
             img_full_path = os.path.join(img_root, movie_name, img_fname)
 
-            # 如果还未获取到该 movie 的图像尺寸，则读取一下
             if movie_infos[movie_name]['size'] is None:
-                # 读一张帧，获取宽高 (如有必要，可以 try/except 跳过无法读取的帧)
                 with Image.open(img_full_path) as img:
                     width, height = img.size
                 movie_infos[movie_name]['size'] = (width, height)
             else:
-                # 如果已经有 size，就直接用
                 width, height = movie_infos[movie_name]['size']
 
-            # 构造一个稳定的 image_id，防止不同 movie 的 timestamp 冲突
-            # 这里 movie_ids[movie_name] * 1000000 是为了给该 movie 预留足够大的区间
             image_id = movie_ids[movie_name] * 1000000 + timestamp
 
             movie_infos[movie_name]['img_infos'][timestamp] = {
@@ -297,22 +67,19 @@ def csv2COCOJson_dynamic(csv_path, movie_list, img_root, json_path, min_json_pat
                 'width': width,
                 'movie': movie_name,
                 'timestamp': timestamp,
-                'annotations': []  # 用列表存储 annotation，避免被 action_id 覆盖
+                'annotations': []  
             }
 
-        # 从字典中取出当前图像的信息
         img_info = movie_infos[movie_name]['img_infos'][timestamp]
         width = img_info['width']
         height = img_info['height']
 
-        # 计算 bounding box
         box_w = (x2 - x1) * width
         box_h = (y2 - y1) * height
         real_x1 = x1 * width
         real_y1 = y1 * height
         area = box_w * box_h
 
-        # 检查当前 bbox 是否已经存在
         existing_annotation = None
         for annotation in img_info['annotations']:
             if annotation['bbox'] == [
@@ -323,20 +90,17 @@ def csv2COCOJson_dynamic(csv_path, movie_list, img_root, json_path, min_json_pat
                 break
 
         if existing_annotation:
-            # 如果 bbox 存在，添加新的 action_id
             if action_id not in existing_annotation['action_ids']:
                 existing_annotation['action_ids'].append(action_id)
         else:
-            # 构造一个全局唯一的 annotation_id
             ann_id = global_ann_id
             global_ann_id += 1
 
-            # 新增一个 annotation
             img_info['annotations'].append({
                 'id': ann_id,
                 'image_id': img_info['id'],
                 'img_path': os.path.join(movie_name, img_fname),
-                'category_id': 1,  # 假设只有一个 “person”
+                'category_id': 1,  
                 'action_ids': [action_id],
                 'person_id': person_id,
                 'bbox': [round(real_x1, 2), round(real_y1, 2),
@@ -345,7 +109,6 @@ def csv2COCOJson_dynamic(csv_path, movie_list, img_root, json_path, min_json_pat
                 'iscrowd': 0
             })
 
-    # 构造 COCO 风格字典
     tic = time.time()
     print("Writing into json file...")
 
@@ -361,7 +124,6 @@ def csv2COCOJson_dynamic(csv_path, movie_list, img_root, json_path, min_json_pat
         'annotations': []
     }
 
-    # 收集所有图像和注释
     for movie_name, minfo in movie_infos.items():
         for timestamp, info in minfo['img_infos'].items():
             jsondata['images'].append({
@@ -374,12 +136,10 @@ def csv2COCOJson_dynamic(csv_path, movie_list, img_root, json_path, min_json_pat
             })
             jsondata['annotations'].extend(info['annotations'])
 
-    # 写入 JSON 带缩进
     with open(json_path, 'w') as f:
         json.dump(jsondata, f, indent=4)
     print(f"Write json dataset into json file {json_path} successfully.")
 
-    # 写入无缩进版本
     with open(min_json_path, 'w') as fmin:
         json.dump(jsondata, fmin)
     print(f"Write json dataset with no indent into json file {min_json_path} successfully.")
