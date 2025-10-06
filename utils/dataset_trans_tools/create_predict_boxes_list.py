155faff9a15119bd4b8c31113754a9eb7faee62d
Huye-Zhong <108203370+wshr123@users.noreply.github.com>
2025-10-06 11:26:32 +0800

    Update create_predict_boxes_list.py

--- utils/dataset_trans_tools/create_predict_boxes_list.py
+++ utils/dataset_trans_tools/create_predict_boxes_list.py
@@ -1,13 +1,10 @@
 import pandas as pd
 
-# 读取 CSV 文件
 input_file = '/media/zhong/1.0T/zhong_work/zhong_detr/inference_result/cvat.csv'  # 将此替换为你的 CSV 文件路径
 df = pd.read_csv(input_file)
 
-# 将最后一列的所有值替换为 0.99
 df.iloc[:, -1] = 0.99
 
-# 保存修改后的数据到新 CSV 文件
 output_file = '/media/zhong/1.0T/zhong_work/CVB/000058916v001/data/ava_v2.2/ava_test_predit_boxes.csv'  # 将此替换为你想保存的文件名
 df.to_csv(output_file, index=False)
 
