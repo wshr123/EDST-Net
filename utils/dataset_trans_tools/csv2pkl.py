a1d35907687f363d3dcd3082941486a6d03051fa
Huye-Zhong <108203370+wshr123@users.noreply.github.com>
2025-10-06 11:28:22 +0800

    Update csv2pkl.py

--- utils/dataset_trans_tools/csv2pkl.py
+++ utils/dataset_trans_tools/csv2pkl.py
@@ -3,36 +3,27 @@ import pickle
 import numpy as np
 from collections import defaultdict
 
-# 输入的 CSV 标注文件路径
 csv_file_path = "/media/zhong/1.0T/zhong_work/zhong_detr/inference_result/test.csv"
 
-# 输出的 pkl 文件路径
 output_pkl_path = "/media/zhong/1.0T/zhong_work/zhong_detr/inference_result/test.pkl"
 
-# 初始化存储结构
 dense_proposals = defaultdict(list)
 
-# 读取 CSV 文件
 with open(csv_file_path, "r") as f:
     reader = csv.reader(f)
     for row in reader:
-        # 从 CSV 文件中读取字段
         video_id, timestamp, x1, y1, x2, y2, action_id, _= row
         x1, y1, x2, y2 = map(float, [x1, y1, x2, y2])
 
-        frame_number = int(float(timestamp))  # 假设视频是 30 FPS
-        frame_number_str = f"{frame_number:04d}"  # 将帧数格式化为 4 位字符串
+        frame_number = int(float(timestamp)) 
+        frame_number_str = f"{frame_number:04d}"  
         key = f"{video_id},{frame_number_str}"
 
-        # 将候选框和置信度添加到对应的键中
-        # 置信度默认设置为 1.0（可以根据需求修改）
         dense_proposals[key].append([x1, y1, x2, y2, 0.99])
 
-# 转换为 NumPy 数组格式
 for key in dense_proposals:
     dense_proposals[key] = np.array(dense_proposals[key], dtype=np.float32)
 
-# 保存为 .pkl 文件
 with open(output_pkl_path, "wb") as f:
     pickle.dump(dense_proposals, f)
 
